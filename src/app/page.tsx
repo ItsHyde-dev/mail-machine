1021ec4167306edcb5bc0a054a84415e5b8ebf21
himanshu joshi <heyitshyde@gmail.com>
2023-07-15 01:47:42 +0530

    added proper routes and redirects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return <main className="w-full h-full">
-    <section className="flex flex-row items-end p-[40px]">
-      <div className="text-4xl text-white font-mysans font-semibold">Mail Machine v.1.0</div>
-      <section className="flex flex-row gap-[30px] ml-auto">
-        <div className="text-2xl text-white font-mysans font-light">Home</div>
-        <div className="text-2xl text-white font-mysans font-light">About</div>
-        <div className="text-2xl text-white font-mysans font-light">Contact</div>
-      </section>
-    </section>
-    <div className="flex flex-column justify-center absolute left-">
-        <div className="text-5xl text-lilac">
-          Welcome
-        </div>
-    </div>
-  </main>;
+  let isLoggedIn = false;
+
+  if (!isLoggedIn) {
+    redirect('/login')
+  }
+
+  redirect('/home')
 }
+
